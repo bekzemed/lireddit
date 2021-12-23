c88b22e77b7fd57774200503f46bf59725b2212f
bek zemed <bereketzemed@gmail.com>
2021-12-23 15:51:51 +0300

    configure urql server side rendering

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,9 +1,20 @@
+import { withUrqlClient } from 'next-urql';
 import { NavBar } from '../components/NavBar';
+import { usePostsQuery } from '../generated/graphql';
+import createUrqlClient from '../utils/createUrqlClient';
 
-const Index = () => (
-  <>
-    <NavBar />
-    <div>hello world</div>
-  </>
-);
-export default Index;
+const Index = () => {
+  const [{ data }] = usePostsQuery();
+  return (
+    <>
+      <NavBar />
+      <div>hello world</div>
+      {!data ? (
+        <div>loading ...</div>
+      ) : (
+        data.posts.map(p => <div key={p._id}>{p.title}</div>)
+      )}
+    </>
+  );
+};
+export default withUrqlClient(createUrqlClient, { ssr: true })(Index);
