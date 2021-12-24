cec1caa843afd5bbaede3d3263f17b341f18d11a
bek zemed <bereketzemed@gmail.com>
2021-12-24 20:15:08 +0300

    add relation between post and user, add authentication middleware

--- server/src/entities/Post.ts
+++ server/src/entities/Post.ts
@@ -5,8 +5,10 @@ import {
   CreateDateColumn,
   UpdateDateColumn,
   BaseEntity,
+  ManyToOne,
 } from 'typeorm';
 import { Field, ObjectType } from 'type-graphql';
+import { User } from './User';
 
 // to change entity into graphql type we add object type and field to it
 @ObjectType()
@@ -16,6 +18,25 @@ export class Post extends BaseEntity {
   @PrimaryGeneratedColumn()
   _id!: number;
 
+  @Field()
+  @Column()
+  title!: string;
+
+  @Field()
+  @Column()
+  text!: string;
+
+  @Field()
+  @Column({ type: 'int', default: 0 })
+  points!: number;
+
+  @Field()
+  @Column()
+  creatorId: number;
+
+  @ManyToOne(() => User, user => user.posts)
+  creator: User;
+
   @Field(() => String)
   @CreateDateColumn()
   createdAt: Date;
@@ -23,8 +44,4 @@ export class Post extends BaseEntity {
   @Field(() => String)
   @UpdateDateColumn()
   updatedAt: Date;
-
-  @Field(() => String)
-  @Column()
-  title!: string;
 }
