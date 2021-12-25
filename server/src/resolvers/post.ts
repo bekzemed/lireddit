2c26feeb1df3517b07240c3a7d0cc94d3579d017
bek zemed <bereketzemed@gmail.com>
2021-12-25 12:59:32 +0300

    create isAuth middleware and apply on Post resolver

--- server/src/resolvers/post.ts
+++ server/src/resolvers/post.ts
@@ -1,5 +1,24 @@
 import { Post } from '../entities/Post';
-import { Arg, Mutation, Query, Resolver } from 'type-graphql';
+import {
+  Arg,
+  Ctx,
+  Field,
+  InputType,
+  Mutation,
+  Query,
+  Resolver,
+  UseMiddleware,
+} from 'type-graphql';
+import { MyContext } from '../types';
+import { isAuth } from '../middleware/isAuth';
+
+@InputType()
+class PostInput {
+  @Field()
+  title!: string;
+  @Field()
+  text!: string;
+}
 
 @Resolver()
 export class PostResolver {
@@ -17,8 +36,15 @@ export class PostResolver {
 
   // creating post
   @Mutation(() => Post)
-  createPost(@Arg('title') title: string): Promise<Post> {
-    return Post.create({ title }).save();
+  @UseMiddleware(isAuth)
+  createPost(
+    @Arg('input') input: PostInput,
+    @Ctx() { req }: MyContext
+  ): Promise<Post> {
+    return Post.create({
+      ...input,
+      creatorId: req.session.userId,
+    }).save();
   }
 
   // update post
